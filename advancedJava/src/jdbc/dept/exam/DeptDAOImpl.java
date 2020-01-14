4f790905272df248bb2142fe60ed08bcd006839c
sonic247897 <sonic247897@gmail.com>
2020-01-14 17:16:34 +0900

    javawork2

---
+++ advancedJava/src/jdbc/dept/exam/DeptDAOImpl.java
@@ -0,0 +1,43 @@
+package jdbc.dept.exam;
+
+import java.util.ArrayList;
+
+public class DeptDAOImpl implements DeptDAO{
+
+	@Override
+	public int insert(DeptDTO dept) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DeptDTO getDeptInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(DeptDTO dept) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(String deptno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<DeptDTO> getDeptList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<DeptDTO> getMemberList(String deptno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ advancedJava/src/jdbc/dept/exam/MenuUI.java
@@ -0,0 +1,28 @@
+package jdbc.dept.exam;
+
+public class MenuUI {
+	public static void insertMenu() {
+		
+	}
+	
+	public static void getDeptInfoMenu() {
+		
+	}
+	
+	public static void updateMenu() {
+		
+	}
+	
+	public static void deleteMenu() {
+		
+	}
+	
+	public static void getDeptListMenu() {
+		
+	}
+	
+	public static void getMemberListMenu() {
+		
+		
+	}
+}
